fd9d4d8c515082ecb97428716c56c4a230ab4f93
IsakN <isaq.tokyo@gmail.com>
2020-04-24 23:43:08 +0200

    PhysicBasreadDoor gjorde halv Animations door gjord

---
+++ Assets/Door.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    Animator animator;
+
+    bool openNeg, openPos;
+    void OnMouseDown()
+    {
+        float distance = Vector3.Distance(this.transform.position, player.transform.position);
+        if(distance < 10)
+        {
+            if (Vector3.Dot(this.transform.forward, player.transform.forward) < 0)
+            {
+                Debug.Log("Test");
+                if (openPos)
+                {
+                    animator.enabled = true;
+                    animator.SetBool("OpenPos", false);
+                    openPos = false;
+                }
+                else
+                {
+                    animator.SetBool("OpenNeg", true);
+                    openNeg = true;
+                }
+            }
+            else
+            {
+                Debug.Log("Test1");
+                if (openNeg)
+                {
+                    animator.enabled = true;
+                    animator.SetBool("OpenNeg", false);
+                    openNeg = false;
+
+                }
+                else
+                {
+                    animator.SetBool("OpenPos", true);
+                    openPos = true;
+                }
+            }
+        }
+    }
+    void PauseAnimationEvent()
+    {
+        animator.enabled = false;
+    }
+}
