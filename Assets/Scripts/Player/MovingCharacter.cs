7e0e58f546d4e8874f4d678fe046b699eede436c
IsakN <isaq.tokyo@gmail.com>
2020-05-03 22:32:04 +0200

    Fixade Fel 6-7, 9-16 enligt kodgransknign

--- Assets/Scripts/Player/MovingCharacter.cs
+++ Assets/Scripts/Player/MovingCharacter.cs
@@ -77,8 +77,13 @@ public class MovingCharacter : MonoBehaviour
         desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * speed;
         StepAudio();
         AudioJump();
+
+        //With Update and FixedUpdate not always being in sync |= will guarantee that the input is never lost
         desiredJump |= Input.GetButtonDown("Jump") && PlayerStats.jump;
     }
+    /*
+     * FixedUpdate should be responsible for alll physics calculations and changes for the script
+     */
     void FixedUpdate()
     {
         UpdateState();
@@ -110,6 +115,8 @@ public class MovingCharacter : MonoBehaviour
             stepsSinceLastGrounded = 0;
             if (stepsSinceLastJump > 1)
                 jumpPhase = 0;
+
+            // The code saved all ground contacts we hit in a frame so to keep it as an unit vector, the contactNormal needs to be normalized if the groundContact is larger than 0
             if (groundContactCount > 1)
                 contactNormal.Normalize();
         }
@@ -118,7 +125,10 @@ public class MovingCharacter : MonoBehaviour
             contactNormal = Vector3.up;
         }
     }
-
+    /*
+     *  Jump is responsible for calculating how the character would jump after a jump has been decided.
+     *  First it checks what type of jump it is using and then it calculates its force
+     */
     void Jump()
     {
         Vector3 jumpDirection;
@@ -143,11 +153,8 @@ public class MovingCharacter : MonoBehaviour
         float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
         jumpDirection = (jumpDirection + Vector3.up).normalized;
         float alignedSpeed = Vector3.Dot(velocity, jumpDirection);
-
         if (alignedSpeed > 0f)
-        {
             jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
-        }
         velocity += jumpDirection * jumpSpeed;
     }
     void AudioJump()
@@ -165,6 +172,10 @@ public class MovingCharacter : MonoBehaviour
     {
         EvaluateCollision(collision);
     }
+    /* 
+     * To make it so that you cant wall jump unless the powerup is aquired we need to check what angle all the collision the character currently has are.
+     * EvaluateCollision takes in all collision the character has had this frame and checks their normal with the ground and steep angles before saving them in their respective variable
+     */
     void EvaluateCollision(Collision collision)
     {
         float minDot = GetMinDot(collision.gameObject.layer);
@@ -183,6 +194,10 @@ public class MovingCharacter : MonoBehaviour
             }
         }
     }
+    /*
+     * AdjustVelocity changes the angle of the movement compared to the ground normal.
+     * Instead of forcing unity collision from pushing the character upwards a slope the characters forward movement will be angled with the slope instead
+     */
     void AdjustVelocity()
     {
         Vector3 xAxis = ProjectOnContactPlane(transform.right).normalized;
@@ -203,22 +218,18 @@ public class MovingCharacter : MonoBehaviour
 
         velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
     }
-    void StepAudio()
-    {
-        if (OnGround && desiredVelocity.magnitude > 0)
-        {
-            if (!audioStep.isPlaying)
-            {
-                audioStep.Play();
-            }
-        }
-        else
-            audioStep.Stop();
-    }
+
+    /*
+     * ProjectOnContactPlane gets the movement vector on the ground by projecting in on the contact normal
+     */
     Vector3 ProjectOnContactPlane(Vector3 vector)
     {
         return vector - contactNormal * Vector3.Dot(vector, contactNormal);
     }
+    /*
+     *  SnapToGround makes sure that when the character goes over the edge of a slope the characters upwards velocity will not make the character float.
+     *  Instead snap back down to the ground unless a jump was made
+     */
     bool SnapToGround()
     {
         if (stepsSinceLastGrounded > 1 || stepsSinceLastJump <= 2)
@@ -245,11 +256,17 @@ public class MovingCharacter : MonoBehaviour
         }
         return true;
     }
+    /*
+     *  GetMinDot prevents the SnapToGround to snap to anything with above the min angle for ground and stairs
+     */
     float GetMinDot(int layer)
     {
         return (stairsMask & (1 << layer)) == 0 ? minGroundDotProduct : minStairsDotProduct;
     }
-
+    /*
+     * CheckSteepContacts prevents the character from getting stuck in a crevasses.
+     * If they angle of all sides combined in the crevasse is within the allowed ground angle then that is also counted as ground
+     */
     bool CheckSteepContacts()
     {
         if (steepContactCount > 1)
@@ -264,5 +281,17 @@ public class MovingCharacter : MonoBehaviour
             }
         }
         return false;
+    }
+    void StepAudio()
+    {
+        if (OnGround && desiredVelocity.magnitude > 0)
+        {
+            if (!audioStep.isPlaying)
+            {
+                audioStep.Play();
+            }
+        }
+        else
+            audioStep.Stop();
     }
 }
