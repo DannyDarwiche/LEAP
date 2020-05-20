ac10867e4cccdf4ea0881febfd87890644c62391
Mumzel <mumzel99@gmail.com>
2020-05-20 22:02:53 +0200

    Updatering av min ö
    Lägger till crafting table.

---
+++ Assets/Muhammads Ö/C#/CraftingTable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingTable : MonoBehaviour
+{
+    public GameObject White1;
+    public GameObject White2;
+    public GameObject White3;
+    public GameObject Blue1;
+    [SerializeField]
+    public int ID;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject == White1)
+        {
+            Destroy(White1);
+            Destroy(White2);
+            Destroy(White3);
+            Blue1.SetActive(true);
+            //GameEvents.currentInstance.ButtonTriggerOn(ID);
+            //ActivatedDoor.SetActive(true);
+            //Debug.Log("Collision");
+
+        }
+    }
+}
