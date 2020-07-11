a30efb2f622242756dad441419c20e55efc106f4
DannyDarwiche <62639702+DannyDarwiche@users.noreply.github.com>
2020-07-11 15:47:28 +0200

    Pirate Island
    Skull added
    Shipwreck started
    Floating objects started

---
+++ Assets/FloatObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class FloatObject : MonoBehaviour
+{
+    [SerializeField]
+    float waterLevel = 0.0f;
+    [SerializeField]
+    float floatThreshold = 2.0f;
+    [SerializeField]
+    float waterDensity = 0.125f;
+    [SerializeField]
+    float downForce = 4.0f;
+
+    float forceFactor;
+    Vector3 floatForce;
+
+    void FixedUpdate()
+    {
+        forceFactor = 1.0f - ((transform.position.y - waterLevel) / floatThreshold);
+        
+        if(forceFactor > 0.0f)
+        {
+            floatForce = -Physics.gravity * GetComponent<Rigidbody>().mass * (forceFactor - GetComponent<Rigidbody>().velocity.y * waterDensity);
+            floatForce += new Vector3(0.0f, -downForce * GetComponent<Rigidbody>().mass, 0.0f);
+            GetComponent<Rigidbody>().AddForceAtPosition(floatForce, transform.position);
+        }
+    }
+}
