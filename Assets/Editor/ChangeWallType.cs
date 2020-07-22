bb584ec784c9967a72aabf89a764751d09f5c75c
DannyDarwiche <62639702+DannyDarwiche@users.noreply.github.com>
2020-07-22 21:30:55 +0200

    Hub Island
    Building Parts for the box houses

---
+++ Assets/Editor/ChangeWallType.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ChangeWallType : EditorWindow
+{
+    int currentSelectionCount = 0;
+    GameObject currentlySelectedGameobject;
+
+    [MenuItem("Custom Tools/Change Wall Type")]
+    public static void ChangeWall()
+    {
+        LaunchEditor();
+    }
+
+    public static void LaunchEditor()
+    {
+        var editorWin = GetWindow<ChangeWallType>();
+        editorWin.Show();
+    }
+
+    void OnGUI()
+    {
+        GetSelection();
+
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Selection Count: " + currentSelectionCount, EditorStyles.boldLabel);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.EndVertical();
+
+        Repaint();
+    }
+
+    void GetSelection()
+    {
+        currentSelectionCount = 0;
+        currentSelectionCount = Selection.gameObjects.Length;
+
+    }
+}
